e1f64c3c3dcf8f92eb11fc53e7d51f026e3251ea
Lino Juan Jose Mundo Nava <a23300216@ceti.mx>
2025-11-01 00:10:00 -0600

    Archivos del proyecto
    Todos los archivos necesarios para que funcione. Importar o abrir desde netbeans.

---
+++ src/mn23300216/pkg21/MN2330021621.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mn23300216.pkg21;
+
+/**
+ *
+ * @author Daimo
+ */
+public class MN2330021621 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI gui = new GUI();
+        gui.setLocationRelativeTo(null);
+        gui.setTitle("Editor de texto - ahora con mas crema :)");
+        gui.setVisible(true);
+    }
+    
+}
